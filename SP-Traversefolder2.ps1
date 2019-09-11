761a79281969234f1e19211640b33ff6e2306b69
Gabriel <41950410+gciorty@users.noreply.github.com>
2019-09-11 10:07:20 +0100

    Add files via upload

---
+++ SP-Traversefolder2.ps1
@@ -0,0 +1,49 @@
+﻿function ListItems ($FOLDER)
+{
+    $Items = Get-PnPFolderItem -FolderSiteRelativeUrl $FOLDER
+
+	Foreach ($item in $Items)
+	{
+        $iName = $item.Name.replace("%","")  
+		if ($item.GetType().Name -eq "Folder")
+		{
+            $nPath = $FOLDER+"/"+$item.Name
+            $dPath = $nPath.replace("SharePointTest","Resources")
+            echo "$dPath""*** CREATED! ***"
+            $calc = $dPath.Length - $item.Name.Length
+            Add-PnPFolder -Name $iName -Folder $dPath.substring(0,$calc)            
+            ListItems $nPath    
+		}
+		else
+		{
+			$fPath = $FOLDER+"/"+$item.Name
+            $dPath = $fPath.replace("SharePointTest","Resources").replace("% ","").replace("%","")
+            
+            $calc = $dPath.Length - $item.Name.Length
+
+            if ($item.Name.Contains("%"))
+            {
+                $calc = $calc + 1
+            }
+
+            $dPath = $dPath.substring(0,$calc)
+            
+            if ($FOLDER -ne "/SharePointTest/Forms")
+            {
+             Copy-PnPFile -SourceUrl "$fPath" -TargetUrl "$dPath" -OverwriteIfAlreadyExists -Force             
+            }
+            
+            echo "$fPath""$dPath""*** COPIED! ***"
+		}
+	}
+    
+}
+
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path C:\support\output.txt -append
+
+ListItems "/SharePointTest"
+
+Stop-Transcript
